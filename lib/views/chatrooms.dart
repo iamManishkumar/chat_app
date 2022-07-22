c58e1635ba1d27fc0119608494574255b259893f
iamManishkumar <manish97933@gmail.com>
2022-07-22 16:34:20 +0530

    chatApp1

---
+++ lib/views/chatrooms.dart
@@ -0,0 +1,141 @@
+import 'package:chat_app/helper/authenticate.dart';
+import 'package:chat_app/helper/constants.dart';
+import 'package:chat_app/helper/helperfunctions.dart';
+import 'package:chat_app/helper/theme.dart';
+import 'package:chat_app/services/auth.dart';
+import 'package:chat_app/services/database.dart';
+import 'package:chat_app/views/chat.dart';
+import 'package:chat_app/views/search.dart';
+import 'package:flutter/material.dart';
+
+class ChatRoom extends StatefulWidget {
+  @override
+  _ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+  DatabaseMethods databaseMethods = new DatabaseMethods();
+  Stream chatRoomStream;
+
+  Widget chatRoomList() {
+    return StreamBuilder(
+        stream: chatRoomStream,
+        builder: (context, snapshot) {
+    return snapshot.hasData ? ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index){
+              return ChatRoomsTile(
+                snapshot.data.documents[index].data["chatroomId"]
+                    .toString().replaceAll("_", "")
+                    .replaceAll(Constants.myName,""),
+                  snapshot.data.documents[index].data["chatroomId"]
+              );
+
+            }) : Container();
+
+      },
+    );
+  }
+
+
+  @override
+  void initState() {
+    getUserInfo();
+
+    super.initState();
+  }
+
+  getUserInfo() async {
+  Constants.myName = await HelperFunctions.getUserNameSharedPreference();
+  databaseMethods.getUserChats(Constants.myName).then((value) {
+    setState(() {
+      chatRoomStream = value;
+    });
+  });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/images/logo.png",
+          height: 40,
+        ),
+        elevation: 0.0,
+        centerTitle: false,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              AuthService().signOut();
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => Authenticate()));
+            },
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.exit_to_app)),
+          )
+        ],
+      ),
+    floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Search()));
+        },
+      ),
+    );
+  }
+}
+
+class ChatRoomsTile extends StatelessWidget {
+  final String userName;
+  final String chatRoomId;
+
+  ChatRoomsTile(this.userName, this.chatRoomId);
+ //  ChatRoomsTile({this.userName,@required this.chatRoomId});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) => Chat(
+            chatRoomId: chatRoomId,
+          )
+        ));
+      },
+      child: Container(
+        color: Colors.black26,
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: Row(
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                  color: CustomTheme.colorAccent,
+                  borderRadius: BorderRadius.circular(30)),
+              child: Text(userName.substring(0, 1),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'OverpassRegular',
+                      fontWeight: FontWeight.w300)),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Text(userName,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'OverpassRegular',
+                    fontWeight: FontWeight.w300))
+          ],
+        ),
+      ),
+    );
+  }
+}
